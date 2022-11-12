9e5c6d5029b3f2d790a388e89990b22b8219e985
Nerijus <bitininkasn@gmail.com>
2022-11-12 23:19:23 +0200

    Add new project wich calculate structure depth

---
+++ ReizhTechTask2Update/Branch.cs
@@ -0,0 +1,33 @@
+﻿namespace ReizhTechTask2Update
+{
+    public class Branch
+    {
+        public int Id;
+        public List<Branch> child;
+
+        public Branch NewBranch(int Id)
+        {
+            Branch branch = new Branch();
+            branch.Id = Id;
+            branch.child = new List<Branch>();
+            return branch;
+        }
+
+        public int DepthOfStructure(Branch structure)
+        {
+            if (structure == null)
+            {
+                return 0;
+            }
+
+            int maxdepth = 0;
+
+            foreach (Branch currentChild in structure.child)
+            {
+                maxdepth = Math.Max(maxdepth, DepthOfStructure(currentChild));
+            }
+
+            return maxdepth + 1;
+        }
+    }
+}
---
+++ ReizhTechTask2Update/Program.cs
@@ -0,0 +1,22 @@
+﻿using ReizhTechTask2Update;
+
+Branch branch = new Branch();
+
+branch = branch.NewBranch(0);
+
+branch.child.Add(branch.NewBranch(1));
+branch.child.Add(branch.NewBranch(2));
+
+branch.child[0].child.Add(branch.NewBranch(3));
+
+branch.child[1].child.Add(branch.NewBranch(4));
+branch.child[1].child.Add(branch.NewBranch(5));
+branch.child[1].child.Add(branch.NewBranch(6));
+
+branch.child[1].child[0].child.Add(branch.NewBranch(7));
+branch.child[1].child[1].child.Add(branch.NewBranch(8));
+branch.child[1].child[1].child.Add(branch.NewBranch(9));
+
+branch.child[1].child[1].child[0].child.Add(branch.NewBranch(10));
+
+Console.WriteLine($"Structure depth is: {branch.DepthOfStructure(branch)}");
